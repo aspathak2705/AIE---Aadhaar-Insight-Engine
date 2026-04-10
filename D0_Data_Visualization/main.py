5aa6aa315999d3bb5f09d5374b61b128f43373d4
aspathak2705 <137284420+aspathak2705@users.noreply.github.com>
2026-04-10 21:09:25 +0530

    main updated

--- D0_Data_Visualization/main.py
+++ D0_Data_Visualization/main.py
@@ -1,7 +1,8 @@
 from src.merge import process_dataset, save_dataframe
 from src.validate import load_schema, validate_dataframe
-
-
+from src.features import create_basic_features
+from src.features import create_ratio_features, create_volume_features
+from src.features import create_monthly_features
 
 def run_pipeline():
     # Paths
@@ -32,6 +33,23 @@ def run_pipeline():
     )
     bio_df = validate_dataframe(bio_df, "biometric", schema)
     save_dataframe(bio_df, f"{output_path}/biometric_master.csv")
+
+    final_df = create_basic_features(enrol_df,demo_df,bio_df)
+
+    # Create feature tables
+    ratio_df = create_ratio_features(final_df)
+    volume_df = create_volume_features(final_df)
+    # Save them
+    save_dataframe(ratio_df, f"{output_path}/features/ratio_features.csv")
+    save_dataframe(volume_df, f"{output_path}/features/volume_features.csv")
+
+    # Create time-series dataset
+    monthly_df = create_monthly_features(enrol_df, demo_df, bio_df)
+    # Save
+    save_dataframe(
+        monthly_df,
+        f"{output_path}/time_series/monthly_features.csv"
+    )
 
 
 if __name__ == "__main__":
